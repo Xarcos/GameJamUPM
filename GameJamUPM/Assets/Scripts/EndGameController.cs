71dd533311e41b059b57537c228885edee67e3c9
Gonzalo\Gonzalo Paniagua <gonzalopanrac@gmail.com>
2017-05-27 21:15:20 +0200

    EndGame nombres

--- GameJamUPM/Assets/Scripts/EndGameController.cs
+++ GameJamUPM/Assets/Scripts/EndGameController.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.Xml;
+using System.IO;
 public class EndGameController : MonoBehaviour {
 
     [SerializeField] UnityEngine.UI.Text m_scoreText;
     [SerializeField] UnityEngine.UI.InputField m_inputField;
 
+    int randomNameIndex;
+    public string[] dogNames;
     string score;
 
 	// Use this for initialization
 	void Start () {
+        randomNameIndex = Random.Range(0, dogNames.Length);
         score = ScoreManager.SCORE.ToString();
 	}
 
     void Update()
     {
-        m_scoreText.text = "Tu puntuación:\n" + ((m_inputField.text == "") ? "Nombre" : m_inputField.text) + " - "  + score ;
+        m_scoreText.text = "Tu puntuación:\n" + ((m_inputField.text == "") ? dogNames[randomNameIndex] : m_inputField.text) + " - "  + score ;
     }
 
     public void OnContinue_ButtonClick()
